05f8a451becfade6ebf023f699834acb9ada06c2
Karan Singhal <karan1149@gmail.com>
2017-08-09 22:25:46 -0400

    Working accepting requests from the frontend using CORS middleware

--- functions/index.js
+++ functions/index.js
@@ -7,12 +7,22 @@ const functions = require('firebase-functions');
 const firebaseAdmin = require('firebase-admin');
 firebaseAdmin.initializeApp(functions.config().firebase);
 
-const cors = require('cors')({origin: true});
+// TODO set host correctly
+const cors = require('cors')({origin: "http://localhost:8000"});
 
 exports.acceptPhrase = functions.https.onRequest((request, response) => {
-  cors(req, res, () => {
-    console.log(request.body);
-    res.status(200).send();
+  cors(request, response, () => {
+    var phraseObject = request.body;
+    if (phraseObject.boolean == true){
+      if (phraseObject.yes == true){
+        console.log("yes");
+      } else {
+        console.log("no");
+      }
+    } else if (phraseObject.boolean == false){
+      console.log(phraseObject.value);
+    }
+    response.status(200).send();
   });
 
 });
@@ -25,13 +35,6 @@ exports.psychicGuess = functions.https.onRequest((request, response) => {
     // Check if user is initiated
     app.tell('Hello, World!');
   }
-
-  // API.AI actions
-  const WELCOME_ACTION = 'input.welcome';
-  const REQUEST_NAME_PERMISSION_ACTION = 'request_name_permission';
-  const REQUEST_LOC_PERMISSION_ACTION = 'request_location_permission';
-  const READ_MIND_ACTION = 'read_mind';
-  const UNHANDLED_DEEP_LINK_ACTION = 'deeplink.unknown';
 
   const actionMap = new Map();
   actionMap.set('input.welcome', welcomeHandler);
